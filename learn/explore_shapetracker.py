583ff7a38314aa17b3f7e86a9d5d6bae52415c21
Clement Verrier <17490658+cverrier@users.noreply.github.com>
2025-02-19 21:57:02 +0800

    work

--- learn/explore_shapetracker.py
+++ learn/explore_shapetracker.py
@@ -1,7 +1,11 @@
 from tinygrad.shape.shapetracker import ShapeTracker
+from tinygrad.shape.view import View
+
+# a = View.create(shape=(2, 2), strides=(2, 1))
+# idx, valid = a.to_indexed_uops()
+# print(idx.render())
 
 a = ShapeTracker.from_shape((3, 2))
 a = a.permute((1, 0))
 a = a.reshape((3, 2))
-idx, valid = a.to_indexed_uops()
-print(idx.render())
+print(a)
