50597579688e1c6df9b805416c1647e73eb626d3
Clement Verrier <17490658+cverrier@users.noreply.github.com>
2025-02-23 15:25:41 +0800

    work

--- learn/conv.py
+++ learn/conv.py
@@ -1,6 +1,8 @@
 from tinygrad import Tensor
 
 # Recreate what happens in the kernel generated for `Tensor.arange()`.
-N = 16
-a = Tensor.zeros(N - 1).cat(Tensor.ones(N))
-print((a._pool(k_=(N,), stride=1, dilation=1).sum(0) - 1).numpy())
+# N = 16
+# a = Tensor.zeros(N - 1).cat(Tensor.ones(N))
+# print((a._pool(k_=(N,), stride=1, dilation=1).sum(0) - 1).numpy())
+
+print(Tensor.arange(150).numpy())
