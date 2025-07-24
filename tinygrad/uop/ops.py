d88530b33636082ee692f516e28b4d872247a4fa
Clement Verrier <17490658+cverrier@users.noreply.github.com>
2025-07-24 10:44:16 +0200

    work

--- tinygrad/uop/ops.py
+++ tinygrad/uop/ops.py
@@ -255,7 +255,7 @@ class UOp(MathTrait, metaclass=UOpMetaClass):
   @staticmethod
   def range(dtype:DType, end:sint, idx:int): return UOp(Ops.RANGE, dtype=dtype, src=(sint_to_uop(end),), arg=idx)
   def r(self, op:Ops, axis:tuple[int, ...], permute=True):
-    axis = tuple(sorted(axis))
+    axis = tuple(sorted([x for x in axis if resolve(self.shape[x] != 1)]))
     if len(axis) == 0: return self
     # move any non reduce axis before the first reduce axis
     move_early, rest = partition(range(axis[0], len(self.shape)), lambda i: i not in axis and resolve(self.shape[i] != 1))
